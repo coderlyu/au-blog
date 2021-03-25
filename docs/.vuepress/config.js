a28f8840750a9dc9bb589f58ab8a4fb65b800f3a
coderlyu <974257574@qq.com>
2021-03-25 12:42:28 +0800

    feat: seo优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,10 @@ const head = require('./config/head')
 module.exports = {
   theme: 'vdoing',
   title: "阿誉的博客",
-  description: '刘誉的博客',
+  description: '一个关于前端技术分享和学习的个人博客,坚持原创,坚持分享',
   dest: 'webView',
   cache: true,
   base: '/',
-  // base: process.env.NODE_ENV === 'development' ? '/' : '/blog/',
   category: true,
   markdown,
   plugins,
--- webView/assets/js/46.7964bbdb.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{418:function(n,w,o){}}]);
