d68e62b8de8131b64f6b1b8180c7f3e9f3575dc1
coderlyu <974257574@qq.com>
2021-03-28 10:27:58 +0800

    refactor: 修改 git

--- git.sh
+++ git.sh
@@ -3,10 +3,8 @@
 set -e
 # 生成静态文件
 npm run build
-# 进入生成的文件夹
-cd ./webView
-git init
+
 git add -A
 git commit -m '更新blog'
-# 把下面的push命令按照你的情况修改后去掉注释
-git push -f git@github.com:coderlyu/blog.git master
+
+git push origin master
