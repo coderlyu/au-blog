af5087e98ebd69043d32839683cc97ee9988f911
刘誉 <liuyu02@weidian.com>
2024-07-03 11:21:40 +0800

    fix: add javascript

---
+++ javascript/1.curry.js
@@ -0,0 +1,27 @@
+/**
+ * https://bigfrontend.dev/problem/implement-curry
+ */
+
+// const join = (a, b, c) => {
+//     return `${a}_${b}_${c}`
+//  }
+
+//  const curriedJoin = curry(join)
+
+//  curriedJoin(1, 2, 3) // '1_2_3'
+
+//  curriedJoin(1)(2, 3) // '1_2_3'
+
+//  curriedJoin(1, 2)(3) // '1_2_3'
+
+/**
+ * @param { (...args: any[]) => any } fn
+ * @returns { (...args: any[]) => any }
+ */
+function curry(fn) {
+  // your code here
+  return function curryFunc(...args) {
+    if (args.length >= fn.length) return fn.call(this, ...args);
+    return curryFunc.bind(this, ...args);
+  };
+}
