68602e580103b854f408a67bcd664fa746179a3f
coderlyu <974257574@qq.com>
2024-07-15 16:30:59 +0800

    feat: tree

--- javascript/68.dom.tag.js
+++ javascript/68.dom.tag.js
@@ -1,3 +1,22 @@
 /**
  * from
+ * https://bigfrontend.dev/problem/get-DOM-tags
  */
+
+/**
+ * @param {HTMLElement} tree
+ * @return {string[]}
+ */
+function getTags(tree) {
+  // your code here
+  const tags = new Set();
+  dfs(tree);
+  function dfs(node) {
+    if (!node) return;
+    tags.add(node.tagName.toLowerCase());
+    for (const child of node.children) {
+      dfs(child);
+    }
+  }
+  return Array.from(tags);
+}
