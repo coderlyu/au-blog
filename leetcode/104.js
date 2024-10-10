4da0f278b91eddcc0634b23b44aa5266fd6858e3
coderlyu <974257574@qq.com>
2024-10-10 11:15:40 +0800

    code

---
+++ leetcode/104.js
@@ -0,0 +1,19 @@
+/**
+ * from
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/?envType=study-plan-v2&envId=top-interview-150
+ *
+ *
+ *
+ *
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {};
